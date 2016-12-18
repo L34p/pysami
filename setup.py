8f04e4a6fb887671c07456c504daad12ff06f0fa
L34p <hmg0228@gmail.com>
2016-12-18 16:16:26 +0900

    Add chardet lib to setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ if version_info.major < 3:
 	print('Cumulus is not compatible under Python 2.')
 	exit()
 
-from distutils.core import setup
-setup(
-	name='pysami', version='0.1.5',
-	packages=['pysami'], package_dir={ 'pysami': 'src' },
-	author='유성매직', author_email='유성매직 <g6123@cys.wo.tc>',
-	description='Python library and script for converting SAMI file.',
-	url='https://github.com/g6123/pysami', license='MIT License',
+from setuptools import setup, find_packages
+setup (
+        name='pysami',
+        version='0.1.5',
+        author='유성매직',
+        author_email='유성매직 <g6123@cys.wo.tc>',
+        description='Python library and script for converting SAMI file.',
+        packages=find_packages(),
+        install_requires=['chardet'],
+        url='https://github.com/g6123/pysami',
+        license='MIT License',
 ) 
